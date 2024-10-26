ca13b0aac3821b71d09f77f8ca16c31b15ec310b
tamili-santos <160561271+tamili-santos@users.noreply.github.com>
2024-10-25 20:06:08 -0600

    Se agrega tarea 5: Net 8

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using ApiEmpresa.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+string? cadena = builder.Configuration.GetConnectionString("DefaultConnection");
+if (cadena!=null){
+    builder.Services.AddDbContext<Conexiones>(opt =>
+opt.UseMySQL(cadena));
+
+}
+builder.Services.AddControllers();
+
+
+    //opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); esto es para SQLSERVER
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
